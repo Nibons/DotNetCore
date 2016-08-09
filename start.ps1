d67f0334ba45643c3f988b8117cfe70ccfeee35b
nibons <imac5640@gmail.com>
2016-08-09 19:36:26 -0400

    Added Powershell Start stuff

--- start.ps1
+++ start.ps1
@@ -1,2 +1,14 @@
+git add . 
+git commit -m [string](read-host)
+git push 
+
+
 $DM_Name = "docker1"
-$DM_IP = docker-machine IP $DM_Name 
+$command = @"
+git clone git@github.com:Nibons/DotNetCore.git \
+    && cd ./DotNetCore \
+    && git checkout HelloWorld \
+    && docker-compose build ./dockerstuff/. \
+    && docker-compose up
+"@
+docker-machine ssh $DM_Name $command
